79f6350abd082ace2d14a60d1fc00dfd5955ab14
M-A-Saldanha <72634705+M-A-Saldanha@users.noreply.github.com>
2021-09-17 19:22:06 -0300

    Primeira Aula

---
+++ dotnet/primeiraAula/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace primeiraAula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int numeDeVezes = 5;
+
+            for(int i=0; i < numeDeVezes;i++)
+            {
+                Console.WriteLine($"Bem vindo ao curso de .Net {i}");
+            }
+        }
+    }
+}
